26b844ef71f5af9d74f2f0a02cab2ff146ef0bbc
Facundo Grasso <facundofggrasso@gmail.com>
2023-12-07 15:11:00 -0300

    Job: Add update endpoint

---
+++ api/job/updateJob.mjs
@@ -0,0 +1,51 @@
+//#region Dependency list
+import { body, validationResult } from "express-validator";
+import { setError } from "../../utils/error-setter.mjs";
+import { jobModel } from "../../models/job.mjs";
+import { getId } from "../../utils/tools.mjs";
+//#endregion
+
+export async function updateJob(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) setError("Validation failed", 422, errors.array());
+
+    const {
+        id,
+        name,
+        hourPrice,
+        cycleEnd,
+        isFortnightly,
+        companyName,
+        description,
+        address,
+    } = req.body;
+
+    try {
+        const storedJob = await jobModel.findById(id);
+        storedJob = {
+            name,
+            hourPrice,
+            cycleEnd,
+            isFortnightly,
+            companyName,
+            description,
+            address,
+        };
+        await storedJob.save();
+
+        res.status(201).json({
+            id: getId(storedJob),
+            name,
+            hourPrice,
+            cycleEnd,
+            isFortnightly,
+            companyName,
+            description,
+            address,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const validUpdateJob = [body("name").trim().not().isEmpty()];
--- routes/job.mjs
+++ routes/job.mjs
@@ -2,10 +2,11 @@
 import { Router } from "express";
 import { createJob, validCreateJob } from "../api/job/createJob.mjs";
 import { getAllJobs } from "../api/job/getAll.mjs";
+import { updateJob, validUpdateJob } from "../api/job/updateJob.mjs";
 //#endregion
 
 export const router = Router();
 router.get("/get-all", getAllJobs);
 router.put("/create", validCreateJob, createJob);
-router.put("/edit", validCreateJob, createJob);
+router.put("/update", validUpdateJob, updateJob);
 //router.delete("/delete/:jobId", isAuth, deleteJob);
