d3ecd3b560312ced2b909744f0f06fa702f5d10b
Facundo Grasso <facundofggrasso@gmail.com>
2024-01-21 15:45:57 -0300

    Fix auth middleware to handle tokens

--- middlewares/is-auth.mjs
+++ middlewares/is-auth.mjs
@@ -1,53 +1,50 @@
 import { sessionModel } from "../models/session.mjs";
 import { setError } from "../utils/error-setter.mjs";
-import { verifyJWT } from "../utils/jws.mjs";
+import { signJWT, verifyJWT } from "../utils/jws.mjs";
 import { getId } from "../utils/tools.mjs";
 
 export const isAuth = async (req, res, next) => {
     const { accessToken, refreshToken } = req.cookies; //Check, maybe we won't need cookie parser
 
-    if (!accessToken) setError("Not authenticated", 401);
-
     try {
-        const { payload, expired } = verifyJWT(accessToken);
+        if (accessToken) {
+            const { payload, expired } = verifyJWT(accessToken);
 
-        // For a valid access token
-        if (payload) req.user = payload;
-
-        if (!req.user) {
-            // expired but valid refresh token
-            const { payload: refresh } =
-                expired && refreshToken
-                    ? verifyJWT(refreshToken)
-                    : { payload: null };
-
-            //This means both tokens are expired, so you can't access the site
-            //I think it makes sense to delete the session from the DB here,
-            //because if we force the user to log in again,
-            //a new session will be created in the DB, making the expired one useless.
-            if (refresh) {
-                const session = await sessionModel.findById(refresh.sessionId);
-
-                if (session?.isValid) {
-                    const newTokenPayload = {
-                        email: session.email,
-                        userId: session.userId,
-                        sessionId: getId(session._id),
-                    };
-                    const newAccessToken = signJWT(
-                        newTokenPayload,
-                        secret,
-                        "1h"
-                    );
-
-                    res.cookie("accessToken", newAccessToken, {
-                        maxAge: 60000 * 60,
-                        httpOnly: true,
-                    });
-                    req.user = newTokenPayload;
-                }
+            if (payload) {
+                req.user = payload;
+                return next();
             }
         }
+
+        if (!refreshToken) setError("Token missing", 401);
+
+        const { payload, expired } = verifyJWT(refreshToken);
+
+        if (expired) {
+            console.log(
+                "I should delete the session from the DB here. Can I do that? I would need the sessionId somehow"
+            );
+            setError("Token expired", 401);
+        }
+
+        if (!payload) setError("Token missing", 401);
+
+        const session = await sessionModel.findById(payload.sessionId);
+        if (!session?.isValid) setError("Session is not valid", 401);
+
+        const newTokenPayload = {
+            email: session.email,
+            userId: session.userId,
+            sessionId: getId(session._id),
+        };
+        const newAccessToken = signJWT(newTokenPayload, "1h");
+
+        res.cookie("accessToken", newAccessToken, {
+            SameSite: "None",
+            maxAge: 60000 * 60,
+            secure: true,
+        });
+        req.user = newTokenPayload;
     } catch (error) {
         next(error);
     }
