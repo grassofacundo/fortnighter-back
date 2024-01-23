0d601a34c12ddffa1cb212549cc1cb2d39f33e29
Facundo Grasso <facundofggrasso@gmail.com>
2024-01-23 15:34:45 -0300

    Create a method to remove empty objects from mongo results

---
+++ utils/objectCleaner.mjs
@@ -0,0 +1,15 @@
+export function removeEmptyProperties(object) {
+    const objectKeys = Object.keys(object);
+    for (let index = 0; index < objectKeys.length; index++) {
+        const item = object[objectKeys[index]];
+        const itemIsObject = typeof item === "object";
+        const itemPropertiesAreEmpty = itemIsObject
+            ? Object.keys(item).length === 0 ||
+              Object.values(item).every((i) => i == null)
+            : false;
+        if (itemPropertiesAreEmpty) object[objectKeys[index]] = undefined;
+        if (itemIsObject && !itemPropertiesAreEmpty)
+            removeEmptyProperties(item);
+    }
+    return object;
+}
