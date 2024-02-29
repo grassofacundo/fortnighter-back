53f36413efbef638b27747fb6cfa56a47259ad98
Facundo Grasso <facundofggrasso@gmail.com>
2024-02-29 14:44:24 -0300

    Setting to None again

--- utils/cookie-helper.mjs
+++ utils/cookie-helper.mjs
@@ -1,7 +1,7 @@
 export function getCookieProperties(maxAge) {
     return {
         maxAge: maxAge ?? 60000 * 60,
-        SameSite: "none",
+        SameSite: "None",
         Secure: true,
         //domain: process.env.FRONT_URL,
     };
